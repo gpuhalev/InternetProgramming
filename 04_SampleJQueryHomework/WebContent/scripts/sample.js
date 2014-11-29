1d47355e6046bc90cf368465732146a8901318fb
gpuhalev <gesho.puh@gmail.com>
2014-11-29 19:10:16 +0200

    First Jquery Homework part 1

---
+++ 04_SampleJQueryHomework/WebContent/scripts/sample.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#button").click(function(){
+		var name = $("#name").val();
+		if (name.length > 0) {
+			$("#list1").append("<li class='pretty'>"+name+"</li>");
+		}else{
+			alert("Please, input a valid name for list element");
+		}
+	   	
+	});
+});
